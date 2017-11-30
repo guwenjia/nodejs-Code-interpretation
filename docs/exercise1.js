536eef97e36dbe66a3e27e9168bf4715fe37aced
谷文佳 <1023478561@qq.com>
2017-11-30 16:19:05 +0800

    案例一

--- docs/exercise1.js
+++ docs/exercise1.js
@@ -1,1 +1,9 @@
+//将myfile中的数据转换为hex编码格式 输出
+var fs   = require('fs')
+var zlib = require('zlib')
+var strs = require('stringstream')
 
+
+var hexStream = fs.createReadStream('myFile').pipe(strs('hex'));
+
+hexStream.pipe(process.stdout)
