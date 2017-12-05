09a799bcaf9bff1b68e928a5376b572533b1fa8c
谷文佳 <1023478561@qq.com>
2017-12-05 16:18:36 +0800

    Create exercise3.js

---
+++ docs/exercise3.js
@@ -0,0 +1,9 @@
+//base64
+var stream = fs.createReadStream('myFile').pipe(strs('base64'))
+var base64Str = ''
+
+stream.on('data', function(data) { base64Str += data })
+stream.on('end', function() {
+     console.log('My base64 encoded file is: ' + base64Str) // Wouldn't work with setEncoding()
+     console.log('Original file is: ' + new Buffer(base64Str, 'base64'))
+})
