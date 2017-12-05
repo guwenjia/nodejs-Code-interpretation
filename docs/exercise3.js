8c03858dcd3a5fd6f0e64fcbdec4ec0c7a918ab0
谷文佳 <1023478561@qq.com>
2017-12-05 16:22:30 +0800

    Update exercise3.js

--- docs/exercise3.js
+++ docs/exercise3.js
@@ -3,11 +3,16 @@ var fs   = require('fs')
 var zlib = require('zlib')
 var strs = require('stringstream')
 
-var stream = fs.createReadStream('myFile').pipe(strs('base64'))
-var base64Str = ''
+var stream = fs.createReadStream('myFile').pipe(strs('base64'))  //myFile里的内容是： hi how are you?
+var base64Str = 'aGVsbG8='  //hello
 
 stream.on('data', function(data) { base64Str += data })
 stream.on('end', function() {
      console.log('My base64 encoded file is: ' + base64Str) // Wouldn't work with setEncoding()
      console.log('Original file is: ' + new Buffer(base64Str, 'base64'))
 })
+
+//输出：                        
+My base64 encoded file is: aGVsbG8=aGkgaG93IGFyZSB5b3U/Cg==   
+Original file is: hello           //??为什么不是 hellohi how are you?
+
